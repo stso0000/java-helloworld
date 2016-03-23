d48df1747babf12bab87696fee5fc53805516e3e
Stanley Tso <stso@hugeinc.com>
2016-03-23 17:06:21 -0400

    Correcting error.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,10 @@ import java.io.*;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		if (args.length == 1) {
-			System.out.printf("Hello there, %s!\n", args[0]);
-		} else {
+		if (args.length == 0) {
 			System.out.printf("Hello there!\n");
+		} else {
+			System.out.printf("Hello there, %s!\n", args[args.length-1]);
 		}
 	}
 }
