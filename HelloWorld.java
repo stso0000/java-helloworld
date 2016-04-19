e0a822001a5289d9c5e974c4bd0c14d6bd5f8840
Stanley Tso <stso@hugeinc.com>
2016-04-19 16:30:44 -0400

    Fixing error.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		if (args.length == 1) {
+		if (args.length == 0) {
 			System.out.printf("Hello there!\n");
 		} else {
 			System.out.printf("Hello there, %s!\n", args[args.length-1]);
